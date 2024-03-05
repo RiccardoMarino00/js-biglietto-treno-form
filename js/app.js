fb76190d70be0d512cb606e1be1835bad614d25a
RiccardoMarino00 <riccardo.marino09@gmail.com>
2024-03-05 16:45:35 +0100

    Added: recap info in html

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@
 const chilometriDaPercorrere = document.getElementById("chilometri");
 const anniPasseggero = document.getElementById("anni").value;
 const bottoneInvia = document.getElementById("button");
+const prezzoBaseElement = document.getElementById("prezzobase");
+const scontoApplicatoElement = document.getElementById("scontoapplicato");
+const prezzoFinaleElement = document.getElementById("prezzofinale");
+
 
 
 bottoneInvia.addEventListener("click", function () {
@@ -25,6 +29,12 @@ bottoneInvia.addEventListener("click", function () {
         const scontoMinorenne = prezzoIntero * 0.20
         const prezzoScontatoMinorenne = prezzoIntero - scontoMinorenne
         console.log("Il prezzo del biglietto scontato per minorenne è " + prezzoScontatoMinorenne.toFixed(2) + " Euro");
+        // stampa prezzi su html
+        prezzoBaseElement.innerHTML = "Il prezzo intero del biglietto è " + prezzoIntero.toFixed(2) + " Euro"
+        scontoApplicatoElement.innerHTML = "Lo sconto del biglietto è " + scontoMinorenne.toFixed(2) + " Euro"
+        prezzoFinaleElement.innerHTML = "Il prezzo scontato del biglietto è " + prezzoScontatoMinorenne.toFixed(2) + " Euro"
+
+
       }
     // Prezzo biglietto scontato over
       // calcolo sconto (prezzo intero * 0.20) e calcolo prezzo scontato over (prezzo intero - sconto over)
@@ -32,11 +42,22 @@ bottoneInvia.addEventListener("click", function () {
         const scontoOver = prezzoIntero * 0.40
         const prezzoScontatoOver =  prezzoIntero - scontoOver
         console.log("Il prezzo del biglietto scontato per over 65 è " + prezzoScontatoOver.toFixed(2) + " Euro");
+        // stampa prezzi su html
+        prezzoBaseElement.innerHTML = "Il prezzo intero del biglietto è " + prezzoIntero.toFixed(2) + " Euro"
+        scontoApplicatoElement.innerHTML = "Lo sconto del biglietto è " + scontoOver.toFixed(2) + " Euro"
+        prezzoFinaleElement.innerHTML = "Il prezzo scontato del biglietto è " + prezzoScontatoOver.toFixed(2) + " Euro"
+
 
       } else{
         console.log("Il prezzo del biglietto è " + prezzoIntero.toFixed(2) + " Euro");
+        // stampa prezzi su html
+        prezzoBaseElement.innerHTML = "Il prezzo intero del biglietto è " + prezzoIntero.toFixed(2) + " Euro"
+
+
 
       }
+
+
       
 })
   
